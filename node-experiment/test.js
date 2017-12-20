df103c53113b51f0ba72c253b09d002c3f37caf4
ywCN <wangyouhao90@gmail.com>
2017-12-20 17:40:04 -0500

    test

--- node-experiment/test.js
+++ node-experiment/test.js
@@ -1,3 +1,9 @@
-console.log("hi");
-
-console.log(2 + 1);
+// create a file
+const fs = require('fs');
+fs.writeFile(__dirname + "/index.html", "<hi>hi</h1>", error => {
+    if (error) {
+        return console.log(error);
+    } else {
+        return console.log('hi');
+    }
+});
