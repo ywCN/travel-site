01031311c288941f9baa202db2e3dd7ccd41135d
ywCN <wangyouhao90@gmail.com>
2017-12-20 17:55:21 -0500

    get pic

--- node-experiment/test.js
+++ node-experiment/test.js
@@ -1,9 +1,16 @@
-// create a file
+// create a file with image
 const fs = require('fs');
+const http = require('http');
+
 fs.writeFile(__dirname + "/index.html", "<h1>hi</h1>", error => {
     if (error) {
         return console.log(error);
     } else {
         return console.log('succeed!');
     }
+});
+
+const photoLocation = 'http://keenthemes.com/preview/metronic/theme/assets/global/plugins/jcrop/demos/demo_files/image1.jpg';
+http.get(photoLocation, response => {
+    response.pipe(fs.createWriteStream(__dirname + '/mydog.jpg'));
 });
