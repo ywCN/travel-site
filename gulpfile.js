d010b84be25d040f80f5f6b39d7079208b071229
ywCN <wangyouhao90@gmail.com>
2017-12-20 19:38:16 -0500

    replacd with ES5 file

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,26 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
+var gulp = require('gulp'),
+watch = require('gulp-watch');
 
-gulp.task('default', () => {
-    console.log('created a gulp task');
+gulp.task('default', function() {
+  console.log("Hooray - you created a Gulp task.");
 });
 
-gulp.task('html', () => {
-    console.log('Imagine something usefull being done to your HTML.');
+gulp.task('html', function() {
+  console.log("Imagine something useful being done to your HTML here.");
 });
 
-gulp.task('watch', () => {
-    watch('./app/index.html', () => {
-        gulp.setMaxListeners('html');
-    });
+gulp.task('styles', function() {
+  console.log("Imagine Sass or PostCSS tasks running here.");
+});
+
+gulp.task('watch', function() {
+
+  watch('./app/index.html', function() {
+    gulp.start('html');
+  });
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('styles');
+  });
+
 });
