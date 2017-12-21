f8a1efd89d2695e487afddaeed47d846baff2a5e
ywCN <wangyouhao90@gmail.com>
2017-12-20 19:30:30 -0500

    cannot use const

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
-const gulp = require('gulp');
+var gulp = require('gulp');
 
 gulp.task('default', () => {
     console.log('created a gulp task');
+});
+
+gulp.task('html', () => {
+    console.log('Imagine something usefull being done to your HTML.');
 });
