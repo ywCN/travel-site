ddeb5a0c97f6adb20be3779b74b98c8f130d15ef
ywCN <wangyouhao90@gmail.com>
2017-12-22 13:28:46 -0500

    finished svgmod

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -7,6 +7,13 @@ svg2png = require('gulp-svg2png');
 var config = {
   mode: {
     css: {
+      variables: {
+        replaceSvgWithPng: function() {
+          return function(sprite, render) {
+            return render(sprite).split('.svg').join('.png');
+          }
+        }
+      },
       sprite: 'sprite.svg',
       render: {
         css: {
