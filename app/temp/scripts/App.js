37adedf1086fdf674a20f9834a587de2d41978d6
ywCN <wangyouhao90@gmail.com>
2017-12-22 10:10:20 -0500

    menu style

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -78,6 +78,7 @@
 	  function MobileMenu() {
 	    _classCallCheck(this, MobileMenu);
 
+	    this.siteHeader = (0, _jquery2.default)(".site-header");
 	    this.menuIcon = (0, _jquery2.default)(".site-header__menu-icon");
 	    this.menuContent = (0, _jquery2.default)(".site-header__menu-content");
 	    this.events();
@@ -92,6 +93,7 @@
 	    key: "toggleTheMenu",
 	    value: function toggleTheMenu() {
 	      this.menuContent.toggleClass("site-header__menu-content--is-visible");
+	      this.siteHeader.toggleClass("site-header--is-expanded");
 	    }
 	  }]);
 
