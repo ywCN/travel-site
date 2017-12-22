85611c64350f3e7827219c9fae2f4b9f7bfb602b
ywCN <wangyouhao90@gmail.com>
2017-12-22 10:35:41 -0500

    added styles for reveal and unreveal elements

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,5 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 var mobileMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll();
