afc282adc27b1d81d12a7ba69466b668b2fbb047
ywCN <wangyouhao90@gmail.com>
2017-12-21 23:10:52 -0500

    install and use webpacl

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,13 +1,10 @@
-function Person(fullName, favColor) {
-  this.name = fullName;
-  this.favoriteColor = favColor;
-  this.greet = function() {
-    console.log("Hello, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
-  }
-}
+var $ = require('jquery');
+var Person = require('./modules/Person');
 
 var john = new Person("John Doe", "blue");
 john.greet();
 
 var jane = new Person("Jane Smith", "green");
 jane.greet();
+
+$("h1").remove();
